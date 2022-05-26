38c138dee5e581424fe84eddfac668d869f84d25
Lee Lawton <lee@attaro.co.uk>
2022-05-26 20:16:00 +0100

    css link fixed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,6 +10,6 @@
 import "@popperjs/core/dist/umd/popper.min"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min"
-import "../allelements-frontend/src/components/index.css"
+import "./src/components/index.css"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
